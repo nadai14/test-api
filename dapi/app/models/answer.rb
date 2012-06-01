858518fb282529aaa8856117f78adeb52adcb42d
yasuhiroishikawa <yas-ishikawa@nadai.jp>
2012-06-01 14:34:26 +0900

    make migration files

--- dapi/app/models/answer.rb
+++ dapi/app/models/answer.rb
@@ -1,3 +1,5 @@
 class Answer < ActiveRecord::Base
   attr_accessible :answer, :enq_id, :num, :update_date, :update_name
+  belongs_to: enq
+  belongs_to: enq_question
 end
--- dapi/app/models/enq_page.rb
+++ dapi/app/models/enq_page.rb
@@ -1,3 +1,5 @@
 class EnqPage < ActiveRecord::Base
   attr_accessible :description, :enq_id, :face, :next_page_id, :page_id, :update_date, :update_name, :wait_until
+  belongs_to: enq
+  has_many: branches
 end
