5c1fa494b8a3dc758974bd01446b8b39531b1bff
gakuzzzz <gaku_github@t2v.jp>
2012-06-03 19:49:37 +0900

    removing an unnecessary method

--- app/controllers/enqs_controller.rb
+++ app/controllers/enqs_controller.rb
@@ -2,11 +2,6 @@
 
 class EnqsController < ApplicationController
 
-  def index
-    a = EnqFace.all
-    render :json => a.to_json
-  end
-
   def show
     enq = Enq.find_by_uuid(params[:id])
     render :json => enq.to_json(:include => :enq_faces)
