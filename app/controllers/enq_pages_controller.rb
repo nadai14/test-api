a75e0aa465d4e8726cb72ff69270b0ed5c4a143e
gakuzzzz <gaku_github@t2v.jp>
2012-06-03 19:50:24 +0900

    adding the index method

--- app/controllers/enq_pages_controller.rb
+++ app/controllers/enq_pages_controller.rb
@@ -1,6 +1,12 @@
 # coding: utf-8
 
 class EnqPagesController < ApplicationController
+
+  def index
+    a = EnqFace.all
+    render :json => a.to_json
+  end
+
   def show
     msg = "<html><body><h1>Hello</h1><p>#{params[:enq_id]}/#{params[:id]}</p></body></html>"
     render :text => msg
