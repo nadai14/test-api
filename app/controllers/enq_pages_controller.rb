058225f2875b40c619dd2cd436f06572ade942e2
gakuzzzz <gaku_github@t2v.jp>
2012-06-06 02:46:33 +0900

    impelementing the error handlers

--- app/controllers/enq_pages_controller.rb
+++ app/controllers/enq_pages_controller.rb
@@ -8,6 +8,7 @@ class EnqPagesController < ApplicationController
   end
 
   def show
+    raise NotFoundException.new "hogeee"
     msg = "<html><body><h1>Hello</h1><p>#{params[:enq_id]}/#{params[:id]}</p></body></html>"
     render :text => msg
   end
