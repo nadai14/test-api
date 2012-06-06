efed3b345e301489a495d233b5d35ffbd20a82bf
yasuhiroishikawa <yas-ishikawa@nadai.jp>
2012-06-06 14:54:01 +0900

    add exp

--- dapi/config/routes.rb
+++ dapi/config/routes.rb
@@ -2,6 +2,8 @@
 Dapi::Application.routes.draw do
   scope "api/v1" do
     # アンケート情報取得URL
+	# ~/api/v1/enqs/:enq.uuid
+	# ~/api/v1/enqs/:enq.uuid?face=:enq_face
     get 'enqs/:uuid' => 'enqs#show'
 	
 	# 動作はshowのみ
