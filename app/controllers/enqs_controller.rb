f7c4541276600bcd4dcc489ac67513a55500eb88
yasuhiroishikawa <yas-ishikawa@nadai.jp>
2012-06-07 13:14:07 +0900

    temp get sessionID

--- app/controllers/enqs_controller.rb
+++ app/controllers/enqs_controller.rb
@@ -5,26 +5,34 @@ class EnqsController < ApplicationController
   # GET /enqs/1.json
   # アンケート情報取得機能
   def show
-    enq_face = params[:face]
+    begin
+      enq_face = params[:face]
 
-    enq = Enq.find_by_uuid(params[:id],
-	                       :include => :enq_faces,		# アンケートフェイスからの情報を取得
-                           :conditions => ["enq_faces.face = ?", enq_face]	# フェイスを条件として検索する
-                          )
+      enq = Enq.find_by_uuid(params[:id],
+	                         :include => :enq_faces,		# アンケートフェイスからの情報を取得
+                             :conditions => ["enq_faces.face = ?", enq_face]	# フェイスを条件として検索する
+                            )
 
-	# セッションIDの取得
-	session_id = self.cookies_session_id
+      # 指定した条件でアンケートが見つからなかった時 NotFoundException を投げる
+      raise NotFoundException.new ENQ_DOES_NOT_EXIST unless enq
+      # アンケートの状態が入稿前、もしくは終了、または終了日時を過ぎていた時、ForbiddenException を投げる
+	  raise ForbiddenException.new BEFORE_OPENING if enq.status == 0
+	  raise ForbiddenException.new AFTER_CLOSING if enq.status == 9 or (enqs.closing_at != null and enqs.closing_at > Time.now)
+
+	  # セッションIDの取得
+	  session_id = self.cookies_session_id
 	
-    # 例外がないときはレンダリング
-    render :json => [enq.to_json(:only => [:id,:movie,:thumbnail,:point,:title,:description,:message,:conversion_tag,:second_picture,:second_point,:client_url],
-                                 :include => {:enq_faces => {:only => [:first_page_id,:wait_until,:css]}}
-                                 ),
-                     session_id.to_json]
+      # 例外がないときはレンダリング
+      render :json => [enq.to_json(:only => [:id,:movie,:thumbnail,:point,:title,:description,:message,:conversion_tag,:second_picture,:second_point,:client_url],
+                                   :include => {:enq_faces => {:only => [:first_page_id,:wait_until,:css]}}
+                                   ),
+                       session_id.to_json]
+    end
   end
-  
+
   def cookies_session_id
     session_id = cookies[:session_id]
-	unless session_id then
+    unless session_id then
       cookies[:session_id] = {:value => UUIDTools::UUID.random_create.to_s}
 	  session_id = cookies[:session_id]
 	end
