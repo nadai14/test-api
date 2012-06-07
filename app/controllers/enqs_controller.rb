f391c4a08f76ddccbf3770152395628819485862
yasuhiroishikawa <yas-ishikawa@nadai.jp>
2012-06-07 12:59:05 +0900

    local method

--- app/controllers/enqs_controller.rb
+++ app/controllers/enqs_controller.rb
@@ -12,20 +12,22 @@ class EnqsController < ApplicationController
                            :conditions => ["enq_faces.face = ?", enq_face]	# フェイスを条件として検索する
                           )
 
-
-
 	# セッションIDの取得
-	#session_id = :cookies_session_id
-    session_id = cookies[:session_id]
-    unless session_id then
-      cookies[:session_id] = {:value => UUIDTools::UUID.random_create.to_s}
-      session_id = cookies[:session_id]
-    end
+	session_id = self.cookies_session_id
 	
     # 例外がないときはレンダリング
     render :json => [enq.to_json(:only => [:id,:movie,:thumbnail,:point,:title,:description,:message,:conversion_tag,:second_picture,:second_point,:client_url],
                                  :include => {:enq_faces => {:only => [:first_page_id,:wait_until,:css]}}
                                  ),
                      session_id.to_json]
+  end
+  
+  def cookies_session_id
+    session_id = cookies[:session_id]
+	unless session_id then
+      cookies[:session_id] = {:value => UUIDTools::UUID.random_create.to_s}
+	  session_id = cookies[:session_id]
+	end
+	session_id
   end
 end
