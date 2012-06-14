cefdc179caf2e460bf7c0bda3e45c6890f75261d
gakuzzzz <gaku_github@t2v.jp>
2012-06-15 04:38:04 +0900

    adding the indexes

--- db/migrate/20120606152918_create_answers.rb
+++ db/migrate/20120606152918_create_answers.rb
@@ -2,13 +2,15 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers, :id => false do |t|
       t.string :uuid, :limit => 36, :primary => true
+      t.string :campaign_id, :null => false
       t.string :enq_question_id, :null => false
-      t.string :session_id, :null => false
+      t.string :user_id, :null => false
       t.string :user_agent, :null => false
       t.text :answer, :null => false
 
       t.string :updated_by
       t.timestamp :updated_at
     end
+    add_index :answers, [:campaign_id, :enq_question_id, :user_id], :unique => true
   end
 end
