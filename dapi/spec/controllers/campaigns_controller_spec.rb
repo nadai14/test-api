cdf4aedee8087c55bb5c2a44b1089cfa15cf3443
yasuhiroishikawa <yas-ishikawa@nadai.jp>
2012-06-16 16:14:00 +0900

    merge gaku-works

--- dapi/spec/controllers/campaigns_controller_spec.rb
+++ dapi/spec/controllers/campaigns_controller_spec.rb
@@ -1,23 +1,25 @@
 #coding: utf-8
 require 'spec_helper'
 require 'support/custom_machers'
+require "json/pure" # gem install json_pure
 
 describe CampaignsController do
   describe "キャンペーン情報取得機能テスト" do
     fixtures :campaigns, :enq_faces, :enqs
     render_views
  
+    before do
+      request.env['X-Requested-By'] = 'poncan-moviereward'
+    end
+
     context "ルートが正しく設定されているか" do
-      before do
-        request.env['HTTP_X_REQUESTED_WITH'] = 'poncan-moviereward'
-      end
 
       describe :routes do
         subject{{:get => "/api/v1/campaigns/1"}}
         it{should route_to(controller: "campaigns", action: "show", id: "1", format: :json)}
       end
       
-      before{get :show, {id: campaigns(:success_confirm).id, face: "TO", format: :json}}
+      before{get :show, {id: campaigns(:success_confirm).id, face: "PC", format: :json}}
       
       describe :response do
         subject{response}
@@ -27,10 +29,7 @@ describe CampaignsController do
 
     context "MIDとフェイスから値を取得する" do
       describe "レスポンスは正しく返ってきているか" do
-        before do
-          request.env['HTTP_X_REQUESTED_WITH'] = 'poncan-moviereward'
-          get :show,{id: campaigns(:success_confirm).id, face: "PC", format: :json}
-        end
+        before {get :show,{id: campaigns(:success_confirm).id, face: "PC", format: :json}}
 
         it 'レスポンスフォーマットの確認' do
           response.should be_success
@@ -50,26 +49,19 @@ describe CampaignsController do
       end
           
       describe "要素が nil の時" do
-        before do
-          request.env['HTTP_X_REQUESTED_WITH'] = 'poncan-moviereward'
-        end
-        
         context "PCからアクセスした場合" do
-          before do
-            @campaign = campaigns(:nullable).point
-            get :show,{id: campaigns(:nullable).id, face: "PC", format: :json}
-          end
+          before {get :show,{id: campaigns(:nullable).id, face: "PC", format: :json}}
 
           context "デフォルト値が設定されていると" do
             def_css = "/css/pc/themes/default/style.css"
             def_title = ""
-            def_desc = '<p>動画を見ながらアンケートに答えてプレゼントをもらおう！</p><p>#{point}ポイントプレゼント</p>'
-            message = '<p>アンケートは終了です。ありがとうございました。</p>'
+            def_desc = JSON.utf8_to_json_ascii('<p>動画を見ながらアンケートに答えてプレゼントをもらおう！</p><p>#{point}ポイントプレゼント</p>')
+            message = JSON.utf8_to_json_ascii('<p>アンケートは終了です。ありがとうございました。</p>')
             it 'デフォルト値を返しているか' do
-              response.body.should include(def_css).to_s.encode("UTF-8", "ISO-2022-JP")
-              response.body.should include(def_title).to_s.encode("UTF-8", "ISO-2022-JP")
-              response.body.should include(def_desc).to_s.encode("UTF-8", "ISO-2022-JP")
-              response.body.should include(message).to_s.encode("UTF-8", "ISO-2022-JP")
+              response.body.should include(def_css)
+              response.body.should include(def_title)
+              response.body.should include(def_desc)
+              response.body.should include(message)
             end
           end
             
@@ -83,16 +75,13 @@ describe CampaignsController do
         end
 
         context "スマートフォンからアクセスした場合" do
-          before do
-            @campaign = campaigns(:nullable).point
-            get :show,{id: campaigns(:nullable).id, face: "TO", format: :json}
-          end
+          before {get :show,{id: campaigns(:nullable).id, face: "SP", format: :json}}
 
           context "デフォルト値が設定されていると" do
             def_css = "/css/sp/themes/default/style.css"
             def_title = ""
-            def_desc = '<p>動画を見ながらアンケートに答えて#{point}ポイントもらおう</p>'
-            message = '<p>アンケートは終了です。ありがとうございました。</p>'
+            def_desc = JSON.utf8_to_json_ascii('<p>動画を見ながらアンケートに答えて#{point}ポイントもらおう</p>')
+            message = JSON.utf8_to_json_ascii('<p>アンケートは終了です。ありがとうございました。</p>')
             it 'デフォルト値を返しているか' do
               response.body.should include(def_css)
               response.body.should include(def_title)
@@ -115,7 +104,10 @@ describe CampaignsController do
       describe "異常系は動作しているか" do
         context "認可されていない時" do
           describe :response do
-            before{get :show, {id: campaigns(:success_confirm).id, face: "TO", format: :json}}
+            before do
+			  request.env['X-Requested-By'] = 'failed_request'
+			  get :show, {id: campaigns(:success_confirm).id, face: "SP", format: :json}
+			end
             
             it 'status 401(UnauthorizedException) を返す' do
               response.status.should == 401
@@ -123,12 +115,8 @@ describe CampaignsController do
           end
         end
         
-        before do
-          request.env['HTTP_X_REQUESTED_WITH'] = 'poncan-moviereward'
-        end
-        
         context "キャンペーンIDが存在しない時" do
-          before{get :show, {id: "Unknown_id", face: "TO", format: :json}}
+          before{get :show, {id: "Unknown_id", face: "SP", format: :json}}
         
           it 'status 404(NotFoundException) を返す' do
             response.status.should == 404
@@ -136,7 +124,7 @@ describe CampaignsController do
         end
         
         context "status == 0 状態が入稿前の時" do
-          before{get :show, {id: campaigns(:camp_status0).id, face: "TO", format: :json}}
+          before{get :show, {id: campaigns(:camp_status0).id, face: "SP", format: :json}}
         
           it 'status 403(ForbiddenException)　を返す' do
             response.status.should == 403
@@ -144,7 +132,7 @@ describe CampaignsController do
         end
         
         context "status == 1 で状態が終了の時" do
-          before{get :show, {id: campaigns(:camp_status9).id, face: "TO", format: :json}}
+          before{get :show, {id: campaigns(:camp_status9).id, face: "SP", format: :json}}
         
           it 'status 403(ForbiddenException) を返す' do
             response.status.should == 403
@@ -152,7 +140,7 @@ describe CampaignsController do
         end
         
         context "closed_date の時間を超えている" do
-          before{get :show, {id: campaigns(:camp_closed).id, face: "TO", format: :json}}
+          before{get :show, {id: campaigns(:camp_closed).id, face: "SP", format: :json}}
         
           it 'status 403(ForbiddenException) を返す' do
             response.status.should == 403
