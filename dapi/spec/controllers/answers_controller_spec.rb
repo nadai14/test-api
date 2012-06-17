6ea55e1de87e7f36717936f12954ed922e45327f
yasuhiroishikawa <yas-ishikawa@nadai.jp>
2012-06-18 03:04:46 +0900

    answer spec

--- dapi/spec/controllers/answers_controller_spec.rb
+++ dapi/spec/controllers/answers_controller_spec.rb
@@ -34,7 +34,10 @@ describe AnswersController do
         before do
           @answer = ['男性',24]
           @uid = "uid"
-          post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:success_confirm).id,
+          @enq_id = enqs(:status1).id
+          @enq_page_id = enq_pages(:sc_PC_page1).id
+          @campaign_id = campaigns(:success_confirm).id
+          post :create, {enq_id: @enq_id, page_id: @enq_page_id, campaign_id: @campaign_id,
                          session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: @answer[0], answer_2: @answer[1], format: :json}
         end
 
@@ -44,9 +47,13 @@ describe AnswersController do
         end
 
         it 'カラムの確認' do
+          arr = JSON.parse(response.body)
           [:enq_id, :enq_page_id, :campaign_id].each do |key|
-            response.body.should have_json("/#{key}")
+            arr.should have_key("#{key}")
           end
+          arr["enq_id"].should == @enq_id
+          arr["enq_page_id"].should == @enq_page_id
+          arr["campaign_id"].should == @campaign_id
         end
         
         it 'レスポンスの表示(目検用)' do
@@ -54,14 +61,20 @@ describe AnswersController do
         end
         
         it '回答の登録の確認' do
+          enq_question_id = EnqQuestion.find(:all,
+                                      :conditions => ["enq_page_id = ? and num = ?", @enq_page_id, 1]
+                                      )
           result = Answer.find(:all,
-                        :conditions => ["campaign_id = ? and enq_question_id = ? and user_id = ?", campaigns(:success_confirm).id, enq_questions(:scPCquestion1), @uid]
+                        :conditions => ["campaign_id = ? and enq_question_id = ? and user_id = ?", @campaign_id, enq_question_id, @uid]
                         )
           result.length.should == 1
           result[0]["answer"].should == @answer[0].to_s
           
+          enq_question_id = EnqQuestion.find(:all,
+                                      :conditions => ["enq_page_id = ? and num = ?", @enq_page_id, 2]
+                                      )
           result = Answer.find(:all,
-                        :conditions => ["campaign_id = ? and enq_question_id = ? and user_id = ?", campaigns(:success_confirm).id, enq_questions(:scPCquestion2), @uid]
+                        :conditions => ["campaign_id = ? and enq_question_id = ? and user_id = ?", @campaign_id, enq_question_id, @uid]
                         )
           result.length.should == 1
           result[0]["answer"].should == @answer[1].to_s
@@ -69,76 +82,129 @@ describe AnswersController do
       end
       
       describe "異常系は動作しているか" do
-        before do
-          @answer = ['男性',24]
-          @uid = "uid"
-        end
+        context "単一のException" do
+          before do
+            @answer = ['男性',24]
+            @uid = "uid"
+          end
       
-        context "認可されていない時" do
-          describe :response do
-            before do
-              request.env['X-Requested-By'] = 'failed_request'
-              post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:success_confirm).id,
-                             session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: @answer[0], answer_2: @answer[1], format: :json}
-            end
+          context "認可されていない時" do
+            describe :response do
+              before do
+                request.env['X-Requested-By'] = 'failed_request'
+                post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:success_confirm).id,
+                               session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: @answer[0], answer_2: @answer[1], format: :json}
+              end
             
-            it 'status 401(UnauthorizedException) を返す' do
-              response.status.should == 401
+              it 'status 401(UnauthorizedException) を返す' do
+                response.status.should == 401
+              end
+            end
+          end
+        
+          context "アンケートIDが存在しない時" do
+            before{post :create, {enq_id: "failed_id", page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:success_confirm).id,
+                                  session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: @answer[0], answer_2: @answer[1], format: :json}}
+        
+            it 'status 404(NotFoundException) を返す' do
+              response.status.should == 404
+            end
+          end
+        
+          context "ページIDが存在しない時" do
+            before{post :create, {enq_id: enqs(:status1).id, page_id: "failed_id", campaign_id: campaigns(:success_confirm).id,
+                                  session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: @answer[0], answer_2: @answer[1], format: :json}}
+        
+            it 'status 404(NotFoundException)　を返す' do
+              response.status.should == 404
+            end
+          end
+        
+          context "キャンペーンIDとアンケートIDが矛盾している" do
+            before{post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:nullable).id,
+                                  session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: @answer[0], answer_2: @answer[1], format: :json}}
+        
+            it 'status 404(NotFoundException) を返す' do
+              response.status.should == 404
+            end
+          end
+        
+          context "アンケートIDとページIDが矛盾している" do
+            before{post :create, {enq_id: enqs(:nullable).id, page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:success_confirm).id,
+                                  session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: @answer[0], answer_2: @answer[1], format: :json}}
+        
+            it 'status 404(NotFoundException) を返す' do
+              response.status.should == 404
+            end
+          end
+
+          context "アンケートページと回答の設問番号が矛盾している" do
+            before{post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_PC_page3_1).id, campaign_id: campaigns(:success_confirm).id,
+                                  session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: "文章です", format: :json}}
+        
+            it 'status 400(BadRequestException) を返す' do
+              response.status.should == 400
+            end
+          end
+
+          context "数値入力設問の回答が数値でない" do
+            before{post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:success_confirm).id,
+                                  session_id: "session_id", uid: "uid", key: "mixi_uid", answer_1: "男性", answer_2: "validate", format: :json}}
+        
+            it 'status 400(BadRequestException) を返す' do
+              response.status.should == 400
+            end
+          end
+
+          context "必須回答であるのに回答がない" do
+            before{post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:success_confirm).id,
+               session_id: "session_id", uid: "uid", key: "mixi_uid", answer_1: "", answer_2: 24, format: :json}}
+        
+            it 'status 400(BadRequestException) を返す' do
+              response.status.should == 400
             end
           end
         end
-        
-        context "アンケートIDが存在しない時" do
-          before{post :create, {enq_id: "failed_id", page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:success_confirm).id,
-                                session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: @answer[0], answer_2: @answer[1], format: :json}}
-        
-          it 'status 404(NotFoundException) を返す' do
-            response.status.should == 404
-          end
-        end
-        
-        context "ページIDが存在しない時" do
-          before{post :create, {enq_id: enqs(:status1).id, page_id: "failed_id", campaign_id: campaigns(:success_confirm).id,
-                                session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: @answer[0], answer_2: @answer[1], format: :json}}
-        
-          it 'status 404(NotFoundException)　を返す' do
-            response.status.should == 404
-          end
-        end
-        
-        context "キャンペーンIDとアンケートIDが矛盾している" do
-          before{post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:nullable).id,
-                                session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: @answer[0], answer_2: @answer[1], format: :json}}
-        
-          it 'status 404(NotFoundException) を返す' do
-            response.status.should == 404
-          end
-        end
-        
-        context "アンケートIDとページIDが矛盾している" do
-          before{post :create, {enq_id: enqs(:nullable).id, page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:success_confirm).id,
-                                session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: @answer[0], answer_2: @answer[1], format: :json}}
-        
-          it 'status 404(NotFoundException) を返す' do
-            response.status.should == 404
-          end
-        end
 
-        context "アンケートページと回答の設問番号が矛盾している" do
-          before{post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_PC_page3_1).id, campaign_id: campaigns(:success_confirm).id,
-                                session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: "文章です", format: :json}}
+        context "複合したException" do
+          context "アンケートページと回答の設問番号が矛盾している、かつ、数値入力設問の回答が数値でない" do
+            before{post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_SP_page2).id, campaign_id: campaigns(:success_confirm).id,
+                                  session_id: "session_id", uid: @uid, key: "mixi_uid", answer_1: "文章です", format: :json}}
         
-          it 'status 400(BadRequestException) を返す' do
-            response.status.should == 400
+            it 'status 400(BadRequestException) を返す' do
+              response.status.should == 400
+			  JSON.parse(response.body)["message"].should == "回答必須の設問に回答していない"
+            end
           end
-        end
 
-        context "設問種類が数値入力で、文字列が入力される" do
-          before{post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:success_confirm).id,
-             session_id: "session_id", uid: "uid", key: "mixi_uid", answer_1: "男性", answer_2: "validate", format: :json}}
+          context "アンケートページと回答の設問番号が矛盾している、かつ、必須回答であるのに回答がない" do
+            before{post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_SP_page3).id, campaign_id: campaigns(:success_confirm).id,
+               session_id: "session_id", uid: "uid", key: "mixi_uid", answer_2: "", format: :json}}
         
-          it 'status 400(BadRequestException) を返す' do
-            response.status.should == 400
+            it 'status 400(BadRequestException) を返す' do
+              response.status.should == 400
+			  JSON.parse(response.body)["message"].should == "回答必須の設問に回答していない"
+            end
+          end
+
+          context "数値入力設問の回答が数値でない、かつ、必須回答であるのに回答がない" do
+            before{post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:success_confirm).id,
+                                  session_id: "session_id", uid: "uid", key: "mixi_uid", answer_1: "", answer_2: "validate", format: :json}}
+        
+            it 'status 400(BadRequestException) を返す' do
+              response.status.should == 400
+			  JSON.parse(response.body)["message"].should == "回答に誤りが存在している"
+            end
+          end
+		  
+          context "アンケートページと回答の設問番号が矛盾している、かつ、数値入力設問の回答が数値でない、かつ、必須回答であるのに回答がない" do
+            before{post :create, {enq_id: enqs(:status1).id, page_id: enq_pages(:sc_PC_page1).id, campaign_id: campaigns(:success_confirm).id,
+                                  session_id: "session_id", uid: "uid", key: "mixi_uid", answer_3: "男性", answer_5: "validate", format: :json}}
+        
+            it 'status 400(BadRequestException) を返す' do
+              response.status.should == 400
+			  JSON.parse(response.body)["message"].should == "回答必須の設問に回答していない"
+            end
           end
         end
       end
