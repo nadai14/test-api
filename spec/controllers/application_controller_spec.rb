0efc4659ff355bc712a31f27d76ea631f13c23be
yasuhiroishikawa <yas-ishikawa@nadai.jp>
2012-06-17 09:36:02 +0900

    made specs

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -6,37 +6,37 @@ class ApplicationControllerDummy < ApplicationController; end
 describe ApplicationController do
   controller ApplicationControllerDummy do
     def index
-	  head :no_content
-	end
+      head :no_content
+    end
   end
   
   describe "認証機能テスト" do
     before{Rails.env = 'development'}
   
-	context "正常に認証されるか" do
-	  it 'リクエストヘッダを認証しているか' do
+    context "正常に認証されるか" do
+      it 'リクエストヘッダを認証しているか' do
         request.env['X-Requested-By'] = 'poncan-moviereward'
-		get :index
-	    response.should be_success
-	  end
-	end
+        get :index
+        response.should be_success
+      end
+    end
 
-	describe "異常系は動作しているか" do
-	  context "ヘッダに誤りがあった時" do
-	    it 'status 401(UnauthorizedException) を返すか' do
+    describe "異常系は動作しているか" do
+      context "ヘッダに誤りがあった時" do
+        it 'status 401(UnauthorizedException) を返すか' do
           request.env['X-Requested-By'] = 'poncan-movie'
-		  get :index
-	      response.status.should == 401
-	    end
-	  end
-	  
-	  context "サーバ内部で異常があった場合" do
-	    it 'status 500(Internal Server Error) を返すか' do
-          request.env['X-Requested-By'] = 'poncan-moviereward'
-		  get :index
-		  response.status.should == 204
-		end
-	  end
-	end
+          get :index
+          response.status.should == 401
+        end
+      end
+      
+#      context "サーバ内部で異常があった場合" do
+#        it 'status 500(Internal Server Error) を返すか' do
+#          request.env['X-Requested-By'] = 'poncan-moviereward'
+#          get :index
+#          response.status.should == 204
+#        end
+#      end
+    end
   end
 end
