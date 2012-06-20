6f0915b3b58a0872f24850a87aaccd5908f0601e
gakuzzzz <gaku_github@t2v.jp>
2012-06-20 17:32:29 +0900

    changing the question_cnt

--- app/controllers/enq_pages_controller.rb
+++ app/controllers/enq_pages_controller.rb
@@ -8,6 +8,11 @@ class EnqPagesController < ApplicationController
     raise NotFoundException.new PAGE_DOES_NOT_EXIST if @page.nil?
     raise NotFoundException.new ID_MISS_MATCH_PAGE if @page.enq_face.enq_id != params[:enq_id]
     raise DataIncompletedException.new IMVALID_QUESTION if @page.enq_questions.any? {|eq| eq.question.needs_choices? && eq.question.choices.empty?}
+    # TODO: move to the campaigin api
+    @count = EnqQuestion.
+      includes(:enq_page => {:enq_face => :enq}).
+      where('enqs.uuid = ? AND enq_faces.face = ?', @page.enq_face.enq_id, @page.enq_face.face).
+      count(:uuid)
   end
 
   private
