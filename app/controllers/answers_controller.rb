a01291d93cb75b8c978c90fde5c07bce1199c30f
gakuzzzz <gaku_github@t2v.jp>
2012-06-21 02:53:17 +0900

    adding multiple answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -29,7 +29,8 @@ class AnswersController < ApplicationController
   end
 
   def register(content, enq_question_id, campaign_id, uid, user_agent)
-    Answer.create(campaign_id: campaign_id, enq_question_id: enq_question_id, user_id: uid, user_agent: user_agent, answer: content)
+    answer = content.class == Array ? content.join("b") : content
+    Answer.create(campaign_id: campaign_id, enq_question_id: enq_question_id, user_id: uid, user_agent: user_agent, answer: answer)
   end
 
 end
