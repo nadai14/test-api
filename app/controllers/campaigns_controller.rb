9749eefa2bf996cb1d294e16939289b4c658f0fa
gakuzzzz <gaku_github@t2v.jp>
2012-06-21 02:47:59 +0900

    adding the movie mime_type

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,15 +1,25 @@
 # coding: utf-8
 
+require 'pathname'
+
 class CampaignsController < ApplicationController
 
   def show
-    @campaign = Campaign.find_by_mid(params[:id], {
-      :include => {:enq => :enq_faces}, 
-      :conditions => ["enq_faces.face = ?", params[:face]]})
-
+    terminal = params[:terminal].try(:downcase).try(:to_sym) || :pc
+    face = terminal == :iphone || terminal == :android ? "TO" : "PC"
+    @campaign = Campaign.includes(:enq => :enq_faces).where("enq_faces.face = ?", face).find_by_mid(params[:id])
     raise NotFoundException.new CAMPAIGN_DOES_NOT_EXIST unless @campaign
     raise ForbiddenException.new BEFORE_OPENING if @campaign.status == 0 # TODO: delete this line after 6/18
     raise ForbiddenException.new AFTER_CLOSING if @campaign.closed?
+    type = MOVIE_TYPES[terminal]
+    @mime_type = type[:mime_type]
+    @movie = @campaign.movie.nil? ? nil : Pathname(@campaign.movie).sub_ext(type[:suffix]).to_s
   end
+
+  MOVIE_TYPES = {
+    :iphone  => {:mime_type => "application/x-mpegURL", :suffix => ".m3u8"}, 
+    :android => {:mime_type => "video/x-flv",           :suffix => ".flv"},
+    :pc      => {:mime_type => "video/mp4",             :suffix => ".mp4"}
+  }
 
 end
