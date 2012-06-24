3182c1cca8809f9d42903027da07f6d5ec1d0ab4
yasuhiroishikawa <yas-ishikawa@nadai.jp>
2012-06-25 06:44:25 +0900

    20120625 0644

--- dapi/public/js/mr/controller/MobileController.js
+++ dapi/public/js/mr/controller/MobileController.js
@@ -6,9 +6,9 @@
  * @author       Li Minghua
  * @author       George Lu
  * @author       Toshiya TSURU <t_tsuru@sunbi.co.jp>
- * @version      $Id: MobileController.js 347 2012-06-24 10:20:21Z tsuru $
+ * @version      $Id: MobileController.js 354 2012-06-24 13:28:04Z tsuru $
  *
- * Last changed: $LastChangedDate: 2012-06-24 19:20:21 +0900 (日, 24 6 2012) $ by $Author: tsuru $
+ * Last changed: $LastChangedDate: 2012-06-24 22:28:04 +0900 (日, 24 6 2012) $ by $Author: tsuru $
  *
  */
 (function(ns){
@@ -29,6 +29,11 @@
 		 */
 		onAdCurrentTimeChanged: function() {
 			ns.trace(this.typeName + '#onAdCurrentTimeChanged()' + this.wait_until);
+			if(!this.getIsAdEnded() && !this.getIsAlready()){
+				var _left = this.getAdDuration() - Math.floor(this.getAdCurrentTime());
+				// this.models.nav.set('html',   '動画の視聴が完了していません<br/>動画を最後まで再生して<br/>アンケートにお答えください。<br/>(残り：' + _left + ' 秒)');
+				this.models.nav.set('html',   '動画の視聴が完了していません。');
+			}
 		},
 		/**
 		 * isAdPlayingChanged 
@@ -40,6 +45,23 @@
 				this.models.nav.set('html', '動画の視聴が完了していません。');
 			}
 		},
+		/**
+     * setIsAdEnded()
+     */
+    setIsAdEnded: function(ended) {
+    	ns.trace(this.typeName + '#setIsAdEnded(' + ended + ')');
+    	if(this.isAdEnded !== ended) {
+    		if(ended === true) {
+    			// check current time & duration
+    			if(this.getAdCurrentTime() < (this.getAdDuration() - 2)) {
+    				return;
+    			}
+    		}
+    		this.isAdEnded = ended;	
+    		this.trigger('change:isAdEnded');
+    	}
+    	return this;
+    },
 		/**
 		 * isAdPlayingChanged 
 		 */
@@ -70,7 +92,6 @@
 			if(!this.getIsAdEnded() && !this.getIsAlready()) {
 				if(this.getAdCurrentTime() < this.getAdDuration() - 2 /**/) {
 					ns.alert('動画を最後まで再生してアンケートにお答えください。(' + this.getAdCurrentTime() + '/' + this.getAdDuration() + ')');
-					// mediaElement.play();
 				}
 			}
 		}
